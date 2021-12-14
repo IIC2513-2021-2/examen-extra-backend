17674458412acac3a11681e5c760a12a8b6305b0
Sebastián Vicencio <sivicencio@gmail.com>
2021-12-14 09:45:59 -0300

    fix(seeds): fix typo in year in seeds

--- src/seeds/20211214083735-add-expeditions.js
+++ src/seeds/20211214083735-add-expeditions.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         name: 'Challenge Movie Crew',
         startDate: '2021-10-05',
-        endDate: '22021-10-17',
+        endDate: '2021-10-17',
         patch: 'https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/mission_patch_images/soyuz2520ms-19_mission_patch_20210807140848.png',
         description: 'The International Space Station (ISS) is a space station, or a habitable artificial satellite, in low Earth orbit. Its first component was launched into orbit in 1998, with the first long-term residents arriving in November 2000. It has been inhabited continuously since that date. The last pressurised module was fitted in 2011, and an experimental inflatable space habitat was added in 2016. The station is expected to operate until 2030. Development and assembly of the station continues, with several new elements scheduled for launch in 2019. The ISS is the largest human-made body in low Earth orbit and can often be seen with the naked eye from Earth. The ISS consists of pressurised habitation modules, structural trusses, solar arrays, radiators, docking ports, experiment bays and robotic arms. ISS components have been launched by Russian Proton and Soyuz rockets, and American Space Shuttles.',
       },
