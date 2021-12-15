f54178618affde115718a4232314c8a469d861da
Sebastián Vicencio <sivicencio@gmail.com>
2021-12-15 13:43:03 -0300

    feat(expeditions): add expedition detail endpoint (Part II first endpoint)

--- src/routes/api/expeditions.js
+++ src/routes/api/expeditions.js
@@ -6,11 +6,28 @@ const ExpeditionSerializer = new JSONAPISerializer('expeditions', {
   keyForAttribute: 'camelCase',
 });
 
+const ExpeditionDetailSerializer = new JSONAPISerializer('expeditions', {
+  attributes: ['name', 'startDate', 'endDate', 'patch', 'description'],
+  keyForAttribute: 'camelCase',
+});
+
 const router = new KoaRouter();
+
+router.param('id', async (id, ctx, next) => {
+  const expedition = await ctx.orm.expedition.findByPk(id);
+  if (!expedition) ctx.throw(404, "The expedition you are looking for doesn't exist");
+  ctx.state.expedition = expedition;
+  await next();
+});
 
 router.get('api.expeditions.index', '/', async (ctx) => {
   const expeditions = await ctx.orm.expedition.findAll();
   ctx.body = ExpeditionSerializer.serialize(expeditions);
+});
+
+router.get('api.candidates.show', '/:id', async (ctx) => {
+  const { expedition } = ctx.state;
+  ctx.body = ExpeditionDetailSerializer.serialize(expedition);
 });
 
 module.exports = router;
